fbc1a1083fbd3570babbb2086542cb3d8fecf6bf
SMG-2009 <warprolear@gmail.com>
2026-02-11 13:34:27 -0500

    mejora

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,13 +1,13 @@
-class Calculator:
+Valor1=int(input("Ingrese su numero: "))
+operacion=input("Ingrese su operacion: ")
+Valor2=int(input("Ingrese su numero 2: "))
 
-    def sumar(self,a,b):
-        return a+b
+if operacion=="+":
+    print("la suma es", Valor1+Valor2)
+if operacion=="-":
+    print("la resta es", Valor1-Valor2)
+if operacion=="*":
+    print("la multiplicacion es", Valor1*Valor2)
+if operacion=="/":
+    print("la division es", Valor1/Valor2)
 
-    def multiplicar(self,a,b):
-        return a*b
-
-    def resta(self,a,b):
-        return a-b
-
-    def dividir(self,a,b):
-        return a/b
